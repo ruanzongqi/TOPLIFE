7d05e5487f22b6e7aad3f702dcf8986db18c580c
ruanzongqi <1570443523@qq.com>
2018-06-13 21:57:34 +0800

    2018/6/13

--- js/reg.js
+++ js/reg.js
@@ -1,16 +1,20 @@
 function $(id){
 	return document.getElementById(id);
 }
+let arr=[false,false,false];
 window.onload=function(){
 	$("numId").onblur=function(){
 		var str=$("numId").value;
 		if(str==""){
 			$("spanId1").innerHTML="请输入电话号码";
+			arr[0]=false;
 		}else if(checkRegExp("phoneNumer",str)==false){
 			$("spanId1").innerHTML="电话号码格式错误";
+			arr[0]=false;
 		}else{
 			$("spanId1").innerHTML="√";
 			$("spanId1").style.color="green";
+			arr[0]=true;
 		}
 	}
 
@@ -19,9 +23,11 @@ window.onload=function(){
 		var type="username";
 		if(str==""){
 			$("spanId2").innerHTML="请输入用户名";
+			arr[1]=false;
 		}else{
 			$("spanId2").innerHTML="√";
 			$("spanId2").style.color="green";
+			arr[1]=true;
 		}
 		// else if(checkRegExp(type,str)==false){
 		// 	$("spanId2").innerHTML="用户名格式错误";
@@ -32,11 +38,14 @@ window.onload=function(){
 		var str=$("passId").value;
 		if(str==""){
 			$("spanId3").innerHTML="请输入密码";
+			arr[2]=false;
 		}else if(checkRegExp("password",str)==false){
 			$("spanId3").innerHTML="密码格式格式";
+			arr[2]=false;
 		}else{
 			$("spanId3").innerHTML="√";
 			$("spanId3").style.color="green";
+			arr[2]=true;
 		}
 	}
 
@@ -51,24 +60,33 @@ window.onload=function(){
 			$("spanId4").style.color="green";
 		}
 	}
-	$("sub").onclick=function(){
-		var xhr = new XMLHttpRequest();
-		xhr.open("post","reg.php",true);
-		xhr.onreadystatechange = function(){
-			if(xhr.readyState==4 && xhr.status==200){
-				if(xhr.responseText=="the userphone exists"){
-					alert("该电话号码已被注册！");
-				}else if(xhr.responseText=="reg succes"){
-					alert("注册成功！");
-				}else if(xhr.responseText=="reg fail"){
-					alert("网页错误！");
-				}
-			}
+	// $("sub").onclick=function(){
+	// 	var xhr = new XMLHttpRequest();
+	// 	xhr.open("post","reg.php",true);
+	// 	xhr.onreadystatechange = function(){
+	// 		if(xhr.readyState==4 && xhr.status==200){
+	// 			if(xhr.responseText=="the userphone exists"){
+	// 				alert("该电话号码已被注册！");
+	// 			}else if(xhr.responseText=="reg succes"){
+	// 				alert("注册成功！");
+	// 			}else if(xhr.responseText=="reg fail"){
+	// 				alert("网页错误！");
+	// 			}
+	// 		}
+	// 	}
+	// 	xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");		
+	// 	let sendstr = "userphone="+$("numId").value+"&username="+$("usernameId").value+"&userpass="+$("passId").value;
+	// 	xhr.send(sendstr);
+	// }
+}
+function check(event){
+	let evt=event || window.event;
+	if(arr!=[true,true,true]){
+		if(evt.preventDefault()){
+			evt.preventDefault();	
+		}else{
+			evt.returnValue = false;
 		}
-		xhr.setRequestHeader("Content-type","application/x-www-form-urlencoded");		
-		let sendstr = "userphone="+$("numId").value+"&username="+$("usernameId").value+"&userpass="+$("passId").value;
-		xhr.send(sendstr);
 	}
-	
 }
 	
--- js/register.js
+++ js/register.js
@@ -3,20 +3,26 @@ function $(id){
 }
 
 window.onload = function(){
-	$("check").onclick = function(){
-		var xhr = new XMLHttpRequest();
-		
-		xhr.open("get","register.php?userphone="+$("userphone").value,true);//设置
-		
-		xhr.onreadystatechange = function(){
-			if(xhr.readyState==4 && xhr.status==200){
-				if(xhr.responseText=="0"){
-					alert("存在");
-				}else{
-					alert("可以用");
+	$("next").onclick = function(){
+		var str=$("userphone").value;
+		if(str==""){
+			alert("请输入电话号码");
+		}else if(checkRegExp("phoneNumer",str)==false){
+			$("userphone").value="";
+			alert("电话号码格式错误");
+		}else{
+			var xhr = new XMLHttpRequest();
+			xhr.open("get","register.php?userphone="+$("userphone").value,true);
+			xhr.onreadystatechange = function(){
+				if(xhr.readyState==4 && xhr.status==200){
+					if(xhr.responseText=="0"){
+						alert("存在");
+					}else{
+						location.href="reg.html";
+					}
 				}
-			}
-		}		
-		xhr.send();
+			}		
+			xhr.send();
+		}
 	}
 }
